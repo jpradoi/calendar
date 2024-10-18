b47c236fb02c8859004786f6e32dc1115d69e8cd
Shiro <85586365+jpradoi@users.noreply.github.com>
2024-10-18 19:42:56 -0300

    Update wsgi.py

--- calendarV2project/calendarV2/wsgi.py
+++ calendarV2project/calendarV2/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendarV2.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calendarV2project.calendarV2.settings')
 
 application = get_wsgi_application()
